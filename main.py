cd13cfabaed9f6c432e1f7d9efbc60919e4492fa
Kacprucha <karas.kacper@gmail.com>
2024-03-31 12:55:47 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,132 @@
+import pygame
+import numpy as np
+import sys
+import ast
+from math import *
+from my_classes import Point, Figure
+
+WHITE = (255,255,255)
+BLACK = (0,0,0)
+
+SCREEN_WIDTH, SCREEN_HEIGHT = 800, 600
+
+RADIOUS = (5 * pi) / 180
+
+def get_ratation_z_matrix(angle):
+    rotation_z = np.matrix([
+        [cos(angle), -sin(angle), 0, 0],
+        [sin(angle), cos(angle), 0, 0],
+        [0, 0, 1, 0],
+        [0, 0, 0, 1]
+    ])
+    return rotation_z
+
+def get_ratation_y_matrix(angle):
+    rotation_y = np.matrix([
+        [cos(angle), 0, sin(angle), 0],
+        [0, 1, 0, 0],
+        [-sin(angle), 0, cos(angle), 0],
+        [0, 0, 0, 1]
+    ])
+    return rotation_y
+
+def get_ratation_x_matrix(angle):
+    rotation_x = np.matrix([
+        [1, 0, 0, 0],
+        [0, cos(angle), -sin(angle), 0],
+        [0, sin(angle), cos(angle), 0],
+        [0, 0, 0, 1]
+    ])
+    return rotation_x
+
+def get_translation_matrix(t_x, t_y, t_z):
+    rotation_x = np.matrix([
+        [1, 0, 0, t_x],
+        [0, 1, 0, t_y],
+        [0, 0, 1, t_z],
+        [0, 0, 0, 1]
+    ])
+    return rotation_x
+
+def main ():
+
+    pygame.init()
+
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    figure_paths = sys.argv[1:]
+    
+    figures = []
+    for path in figure_paths:
+        points = []
+        with open(path, 'r') as file:
+            for line in file:
+                x,y,z = line.split()
+                point = Point(int(x), int(SCREEN_HEIGHT - int(y)), int(z))
+                points.append(point)
+            
+            figure = Figure(screen, points, 1)
+            figures.append(figure)
+
+    current_zoom = 1
+    running = True
+    while running:
+        for fig in figures:
+            fig.draw_figure()
+    
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            elif event.type == pygame.KEYDOWN:
+                screen.fill(BLACK)
+                if event.key == pygame.K_LEFT:
+                    for fig in figures:
+                        fig.applay_geometric_transformation(get_ratation_z_matrix(RADIOUS))
+                if event.key == pygame.K_RIGHT:
+                    for fig in figures:
+                        fig.applay_geometric_transformation(get_ratation_z_matrix(-RADIOUS))
+                if event.key == pygame.K_e:
+                    for fig in figures:
+                        fig.applay_geometric_transformation(get_ratation_y_matrix(-RADIOUS))
+                if event.key == pygame.K_q:
+                    for fig in figures:
+                        fig.applay_geometric_transformation(get_ratation_y_matrix(RADIOUS))
+                if event.key == pygame.K_z:
+                    for fig in figures:
+                        fig.applay_geometric_transformation(get_ratation_x_matrix(RADIOUS))
+                if event.key == pygame.K_c:
+                    for fig in figures:
+                        fig.applay_geometric_transformation(get_ratation_x_matrix(-RADIOUS))
+                if event.key == pygame.K_a:
+                    for fig in figures:
+                        fig.applay_geometric_transformation(get_translation_matrix(5, 0, 0))
+                if event.key == pygame.K_d:
+                    for fig in figures:
+                        fig.applay_geometric_transformation(get_translation_matrix(-5, 0, 0))
+                if event.key == pygame.K_w:
+                    for fig in figures:
+                        fig.applay_geometric_transformation(get_translation_matrix(0, 5, 0))
+                if event.key == pygame.K_s:
+                    for fig in figures:
+                        fig.applay_geometric_transformation(get_translation_matrix(0, -5, 0))
+                if event.key == pygame.K_UP:
+                    for fig in figures:
+                        fig.applay_geometric_transformation(get_translation_matrix(0, 0, -5))
+                if event.key == pygame.K_DOWN:
+                    for fig in figures:
+                        fig.applay_geometric_transformation(get_translation_matrix(0, 0, 5))
+                if event.key == pygame.K_m:
+                    current_zoom = current_zoom / 2
+                    for fig in figures:
+                        fig.change_zoom(current_zoom)
+                if event.key == pygame.K_p:
+                    current_zoom = current_zoom * 2
+                    for fig in figures:
+                        fig.change_zoom(current_zoom)
+            
+        pygame.display.flip()
+    
+    pygame.quit()
+
+if __name__ == '__main__':
+    main()
---
+++ my_classes.py
@@ -0,0 +1,79 @@
+import pygame
+import numpy as np
+from math import *
+
+LINE_COLOR = (255,255,255)
+D = 1000
+
+projection_matrix = np.matrix([
+    [1, 0, 0, 0],
+    [0, 1, 0, 0],
+    [0, 0, 1, 0],
+    [0, 0, 0, 1]
+])
+
+def get_projection_matrix():
+    projection_m = np.matrix([
+        [1, 0, 0, 0],
+        [0, 1, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 1/D, 1]
+    ])
+    return projection_m
+
+class Point:
+    def __init__(self, x, y, z):
+        self.x = x
+        self.y = y
+        self.z = z
+        
+    def coordinates_as_matrix(self):
+        matrix = np.matrix([self.x, self.y, self.z, 1])
+        r_matrix = matrix.reshape((4,1))
+        return r_matrix
+    
+    def matrix_operation(self, matrix):
+        r_matrix = self.coordinates_as_matrix()
+        operation = np.dot(matrix, r_matrix)
+        self.x = int(operation[0][0])
+        self.y = int(operation[1][0])
+        self.z = int(operation[2][0])
+        
+class Figure:
+    def __init__(self, screen, points_list, zoom):
+        self.screen = screen
+        self.points_list = points_list
+        self.zoom = zoom
+        
+    def connect_points(self, screen, s_point, e_point):
+        s_r_matrix = s_point.coordinates_as_matrix()
+        projection2d = np.dot(get_projection_matrix(), s_r_matrix)
+        # s_x = int(projection2d[0][0] )
+        # s_y = int(projection2d[1][0] )
+        s_x = ((s_point.x * D) / (s_point.z + D)) * self.zoom
+        s_y = ((s_point.y * D) / (s_point.z + D)) * self.zoom
+        
+        e_r_matrix = e_point.coordinates_as_matrix()
+        projection2d = np.dot(get_projection_matrix(), e_r_matrix)
+        # e_x = int(projection2d[0][0] )
+        # e_y = int(projection2d[1][0] )
+        e_x = ((e_point.x * D) / (e_point.z + D)) * self.zoom
+        e_y = ((e_point.y * D) / (e_point.z + D)) * self.zoom
+        
+        pygame.draw.line(screen, LINE_COLOR, (s_x, s_y), (e_x, e_y))
+
+    def draw_figure(self):            
+        for p in range(4):
+            line_d = self.connect_points(self.screen, self.points_list[p], self.points_list[(p+1) % 4])
+            
+            line_u = self.connect_points(self.screen, self.points_list[p + 4], self.points_list[((p+1) % 4) + 4])
+            
+            line_s = self.connect_points(self.screen, self.points_list[p], self.points_list[p + 4])
+            
+    def applay_geometric_transformation(self, matrix):
+        i = 0
+        for point in self.points_list:
+            point.matrix_operation(matrix)
+            
+    def change_zoom(self, zoom):
+        self.zoom = zoom
